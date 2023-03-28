4ee7c61c77547942b4efca7abed44b70a8d7bc9b
Karizmaflores <flores_karizma@yahoo.com>
2023-03-27 20:17:38 -0500

    Solutions are fixed and showing in the DOM

--- dom.js
+++ dom.js
@@ -1,18 +1,12 @@
+//displays current date to DOM
 const displayDate = () => {
   const currentDate = new Date()
 
   document.getElementById("display-element").innerHTML = currentDate;
 }
- 
 
 
-
-
-
-
-
-
-
+//converts number input to string and displays to DOM
 function convertNumToString(){
   let input = document.getElementById("numString").value
   let conversion = input.toString()
@@ -20,12 +14,7 @@ function convertNumToString(){
 }
 
 
-
-
-
-
-
-
+//converts string input to number and displays to DOM
 function stringToNum(){
   let input = document.getElementById("stringNum").value
   let conversion = Number(input)
@@ -35,13 +24,13 @@ function stringToNum(){
 
 
 
-
-// console.log(checkType(1));
-//   console.log(checkType("1"));
-//   console.log(checkType(false));
-//   console.log(checkType(null));
-//   console.log(checkType(undefined));
-//   console.log(checkType(Nan));
+// checks these data types and displays to DOM
+// 1
+// "1"
+// false
+// null
+// undefined
+// NaN
 
 function checkType(){
   let input = document.getElementById("datatypeCheck").value
@@ -72,9 +61,7 @@ function checkType(){
 }
 
 
-
-
-
+//Stores two input string "numbers",  then converts to numbers, adds them together, and displays to DOM
 function addNumbers(){
   let num1 = document.getElementById("num1").value
   let num2 = document.getElementById("num2").value
@@ -86,9 +73,7 @@ function addNumbers(){
 }
 
 
-
-
-
+//Determines is towo input numbers are even and displays to DOM
 function evenNumbers(){
   let thing1 = document.getElementById("thing1").value
   let thing2= document.getElementById("thing2").value
@@ -104,36 +89,34 @@ function evenNumbers(){
 }
 
 
+//Determines if one of the input numbers is even and displays to DOM
+function oneIsTrue(){
+  let one = document.getElementById("one").value
+  let two= document.getElementById("two").value
 
-
-//one or other
-function evenNumbers(){
-  let thing1 = document.getElementById("thing1").value
-  let thing2= document.getElementById("thing2").value
-
-  if(thing1 % 2 === 0 && thing2 % 2 === 0){
-  document.getElementById("display-element6").innerHTML =
-  "Both numbers are even";
+  if(one % 2 === 0 || two % 2 === 0){
+  document.getElementById("display-element7").innerHTML =
+  "One number is even";
   }
   else{
-    document.getElementById("display-element6").innerHTML =
+    document.getElementById("display-element7").innerHTML =
   "Both numbers are NOT even";
   }
 }
 
 
-//both not true
-function evenNumbers(){
-  let thing1 = document.getElementById("thing1").value
-  let thing2= document.getElementById("thing2").value
+//Determines if none of the input numbers are even and displays to DOM
+function noneTrue(){
+  let firstOne = document.getElementById("firstOne").value
+  let secondOne= document.getElementById("secondOne").value
 
-  if(thing1 % 2 !== 0 && thing2 % 2 !== 0){
-  document.getElementById("display-element6").innerHTML =
-  "Both numbers are even";
+  if(firstOne % 2 !== 0 && secondOne % 2 !== 0){
+  document.getElementById("display-element8").innerHTML =
+  "Both numbers are NOT even";
   }
   else{
-    document.getElementById("display-element6").innerHTML =
-  "Both numbers are NOT even";
+    document.getElementById("display-element8").innerHTML =
+  "One number is even";
   }
 }
 
